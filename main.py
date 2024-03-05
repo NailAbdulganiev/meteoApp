6a59417abf50f11f68cc504bf41cfa05a09c7f99
NailAbdulganiev <nail_abdulganiev@mail.ru>
2024-03-05 23:34:12 +0400

    create csv-file with data

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import time
 import requests
 import pandas as pd
-import json
 
 day = 86400
 url = 'https://meteoapi.xn--b1ahgiuw.xn--p1ai/parameter/'
@@ -9,9 +8,9 @@ stationID = {
     "Сервисный центр": '"00001F76"',
     "Отделение 17": '"00001F77"',
     "Отделение 9": '"00001F78"',
-    "Отделение 12": '"00001F7D"',
     "ПУ Север": '"0000235D"',
-    "ПУ Кавказ": '"0000235E"'
+    "ПУ Кавказ": '"0000235E"',
+    "Отделение 12": '"00001F7D"',
 }
 stationParameters = {
     1: '"SOLAR_RADIATION"',  # Солнечная радиация
@@ -31,35 +30,22 @@ try:
         t = int(time.time())
         msg = {
             "meteoId": stationID.get("Сервисный центр").strip('"'),
-            "endTime": t - day,
+            "endTime": t - 15 * day,
             "parameterName": stationParameters.get(parameter).strip('"'),
-            "startTime": t - 3 * day
+            "startTime": t - 40 * day
         }
         response = requests.post(url, json=msg)
         response.raise_for_status()
-        print("Запрос успешно отправлен!")
+        print(f"Request {parameter}/7...")
 
         data = response.json()
         data_dict[stationParameters[parameter]] = data['values']['values']
+    print(f"All requests have been sent successfully!")
 
-        # Создаем DataFrame из полученных данных
-        df = pd.DataFrame(data_dict)
-
-        print(df)
-
-
-    # # Делаем словарь, где время в часах это ключ, а значение параметра в то время - это values
-    # data = response.json()
-    # dates = data['dates']
-    # values = data['values']['values']
-    #
-    # data_dict = dict(zip(dates, values))
-    #
-    # # Выводим данные
-    # print("Ответ сервера:")
-    # print("Dates and values:")
-    # for date, value in data_dict.items():
-    #     print(f"{date}: {value}")
+    # Создаем DataFrame из полученных данных
+    df = pd.DataFrame(data_dict)
+    df.to_csv('meteo_data.csv', index=False)
+    #print(df)
 
 except requests.exceptions.RequestException as e:
     print("Произошла ошибка при отправке запроса:", e)
